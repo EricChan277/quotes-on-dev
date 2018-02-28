6a09498f03b7928daded1bbd9daeacbb882f3803
Eric Chan <ericchan277@gmail.com>
2018-02-28 11:04:22 -0800

    Created submit form

--- page-submit.php
+++ page-submit.php
@@ -8,13 +8,42 @@
 get_header(); ?>
 
 	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+		<main id="main" class="site-main" role="main">      
+      <section class="quotes-submit-wrapper">
+         
+        <header>
+          <?php the_title('<h1 class="entry-title">', '</h1>');?>
+        </header>
 
-			<?php while ( have_posts() ) : the_post(); ?>
 
-				<?php get_template_part( 'template-parts/content', 'page' ); ?>
+        <form name="quoteForm" id="quote-submission-form">
 
-			<?php endwhile; // End of the loop. ?>
+          <div>
+            <label for="quote-author">"Author of Quote"</label>
+            <input type="text" name="quote_author" id="quote-author">
+          </div>
+
+          <div>
+            <label for="quote-content">Quote</label>
+            <textarea name="quote_content" id="quote-content" cols="20"
+            rows="3"></textarea>
+          </div>
+
+          <div>
+            <label for="quote-source">Where did you find this quote? (e.g book name) </label>
+            <input type="text" name="quote_source" id="quote-source">
+          </div>
+
+          <div>
+            <label for="quote-source-url">Provide the URL of the quote source, if applicable.</label>
+            <input type="url" name="quote_source_url" id="quote-source-url">
+          </div>
+
+        </form>
+
+
+      </section>
+
 
 		</main><!-- #main -->
 	</div><!-- #primary -->
