a6cb19edecd8dffef43bea0e0eaeabd9a76e74cf
Eric Chan <ericchan277@gmail.com>
2018-02-28 10:47:31 -0800

    Added logo, created page-submit.php

--- footer.php
+++ footer.php
@@ -11,7 +11,20 @@
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
 				<div class="site-info">
-					<a href="<?php echo esc_url( 'https://wordpress.org/' ); ?>"><?php printf( esc_html( 'Proudly powered by %s' ), 'WordPress' ); ?></a>
+
+				<?php 
+				wp_nav_menu(
+					array(
+						'theme_location' => 'primary',
+						'menu_id' => 'primary-menu',
+						'menu_class' => 'footer-navigation'
+					)
+					);
+					?>
+
+
+				<p><a href="https://github.com/EricChan277/quotes-on-dev">Brought to you by &copy; Eric Chan <?php echo date( 'Y' );?></a></p>
+
 				</div><!-- .site-info -->
 			</footer><!-- #colophon -->
 		</div><!-- #page -->
--- header.php
+++ header.php
@@ -21,15 +21,24 @@
 			<a class="skip-link screen-reader-text" href="#content"><?php esc_html( 'Skip to content' ); ?></a>
 
 			<header id="masthead" class="site-header" role="banner">
-				<div class="site-branding">
-					<h1 class="site-title screen-reader-text"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-					<p class="site-description"><?php bloginfo( 'description' ); ?></p>
+			
+			<div class="site-branding">
+					
+					<div class="logo">
+						<h1 class="site-title screen-reader-text">
+								<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">
+									<?php bloginfo( 'name' ); ?>
+								</a>
+						</h1>
+						
+						<img src="<?php echo get_template_directory_uri() . './assets/qod-logo.svg'; ?>" class="logo" alt="quotes on dev logo">
+
+
+					</div><!--closing logo tag-->
 				</div><!-- .site-branding -->
 
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu' ) ); ?>
-				</nav><!-- #site-navigation -->
 			</header><!-- #masthead -->
 
 			<div id="content" class="site-content">
+
+
