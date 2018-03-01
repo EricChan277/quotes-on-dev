ef69555f2e4cd7ae1aa9b7bb9372b4e14b9465d4
Eric Chan <ericchan277@gmail.com>
2018-03-01 10:57:12 -0800

    Created basic button functionality for reloading

--- js/api.js
+++ js/api.js
@@ -2,9 +2,13 @@
 
 // fetch a random quote post
 
+$("#new-quote-button").on('click',function () {
+  location.reload(); 
+  // console.log('yo i work');
+});
 
 // history api,
 
 // submit a new quote with the form using ajax.
 
-})(jQuery);
+})(jQuery);as
